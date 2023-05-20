37a5c8c823710657b5ae1c7d6fbd7b5fd9fb2e80
Mr-Tejesh <114306366+Mr-Tejesh@users.noreply.github.com>
2023-05-20 12:20:57 +0530

    Add files via upload

---
+++ dicee.js
@@ -0,0 +1,42 @@
+var player1DiceValue = Math.floor((Math.random()*6))+1;
+var player2DiceValue = Math.floor((Math.random()*6))+1;
+if(player1DiceValue === 1){
+    document.querySelector(".img1").setAttribute("src","./images/dice1.png")
+}
+else if(player1DiceValue === 2){
+    document.querySelector(".img1").setAttribute("src","./images/dice2.png")
+}
+else if(player1DiceValue === 3){
+    document.querySelector(".img1").setAttribute("src","./images/dice3.png")
+}
+else if(player1DiceValue === 4){
+    document.querySelector(".img1").setAttribute("src","./images/dice4.png")
+}
+else if(player1DiceValue === 5){
+    document.querySelector(".img1").setAttribute("src","./images/dice5.png")
+}
+else if(player1DiceValue === 6){
+    document.querySelector(".img1").setAttribute("src","./images/dice6.png")
+}
+
+if(player2DiceValue === 1){
+    document.querySelector(".img2").setAttribute("src","./images/dice1.png")
+}
+else if(player2DiceValue === 2){
+    document.querySelector(".img2").setAttribute("src","./images/dice2.png")
+}
+else if(player2DiceValue === 3){
+    document.querySelector(".img2").setAttribute("src","./images/dice3.png")
+}
+else if(player2DiceValue === 4){
+    document.querySelector(".img2").setAttribute("src","./images/dice4.png")
+}
+else if(player2DiceValue === 5){
+    document.querySelector(".img2").setAttribute("src","./images/dice5.png")
+}
+else if(player2DiceValue === 6){
+    document.querySelector(".img2").setAttribute("src","./images/dice6.png")
+}
+
+console.log(player1DiceValue);
+console.log(player2DiceValue);
